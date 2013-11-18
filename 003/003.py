8c000b11b492e2a4c4a8b76c54d7521fde7a1346
Chris Weathers <chris@doubleencore.com>
2013-11-18 09:50:24 -0700

    Progress on 003.

--- 003/003.py
+++ 003/003.py
@@ -1,16 +1,35 @@
-def prime(index):
-	previous_prime = prime(index - 1)
+primes = [1,2,3,5,7]
 
-	prime = previous_prime + 1
-
-	while (is_prime(prime) == False):
-		prime += 1
-
-	return prime
-
-def is_prime(number):
-	for divisor in xrange(1, number):
-		if (number % divisor == 0):
+def number_is_prime(number):
+	for prime in primes:
+		if (prime != 1 and number % prime == 0)
 			return False
 	return True
 
+def next_prime_after_prime(prime):
+	while True:
+		prime += 2
+		if (number_is_prime(prime)):
+			return prime;
+
+def prime_at_index(index):
+	if (index < len(primes)):
+		return primes(index)
+	else
+		previous_index = index - 1
+		previous_prime = primes[previous_index]
+		prime = next_prime_after_prime(previous_prime)
+		primes.append(prime)
+		return prime
+
+def greatest_prime_factor(number):
+	prime_factors = prime_factors(number)
+	greatest_prime_factor = prime_factors[-1]
+	return greatest_prime_factor
+
+def prime_factors(number):
+	for prime in reverse(primes):
+		return
+
+for x in xrange(1,600851475143 / 2):
+	print x
